ebc7f7d347a49681a5005ee2b15a2dfe0f18dd47
tors42 <4084220+tors42@users.noreply.github.com>
2024-08-06 18:20:04 +0200

    Fix check for duplicates (id instead of name)

--- src/main/java/bot/Rules.java
+++ src/main/java/bot/Rules.java
@@ -22,7 +22,7 @@ record Rules(List<Rule> rules) {
                 && ! (e.challenge().gameType().variant() instanceof VariantType.Chess960)
                 && ! (e.challenge().gameType().variant() instanceof VariantType.FromPosition), "Variant", DeclineReason.standard),
             new Rule((_, games) -> games.size() > 8, "Too many games", DeclineReason.later),
-            new Rule((e, games) -> games.containsKey(e.challenge().players().challengerOpt().map(p -> p.user().name()).orElse("")), "Existing game", DeclineReason.later)
+            new Rule((e, games) -> games.containsKey(e.challenge().players().challengerOpt().map(p -> p.user().id()).orElse("")), "Existing game", DeclineReason.later)
             ));
     }
 
